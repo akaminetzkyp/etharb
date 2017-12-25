a96804e36c95c1e0678ea6c5ba63e3dfd13094ce
Alejandro Kaminetzky <akaminetzkyp@gmail.com>
2017-12-25 12:51:51 -0500

    Finish basic script

--- main.py
+++ main.py
@@ -2,9 +2,25 @@ from api_wrappers import CryptoMKT, SurBTC, Pushbullet
 import time
 
 check_delay = 60
-pushbullet = Pushbullet('')
+threshold = 0
+
+pushbullet = Pushbullet('INSERT TOKEN')
 
 while True:
     cryptomkt_prices = CryptoMKT.get_prices()
     surbtc_prices = SurBTC.get_prices()
+
+    print(cryptomkt_prices)
+    print(surbtc_prices)
+
+    if cryptomkt_prices['bid'] - surbtc_prices['ask'] >= threshold:
+        title = 'Arbitrage oportunity!'
+        body = 'CryptoMKT\'s bid is {}CLP greater than SurBTC\'s ask.'.format(
+            threshold)
+        pushbullet.push_note(title, body)
+    elif surbtc_prices['bid'] - cryptomkt_prices['ask'] >= threshold:
+        title = 'Arbitrage oportunity!'
+        body = 'SurBTC\'s bid is {}CLP greater than CryptoMKT\'s ask.'.format(
+            threshold)
+        pushbullet.push_note(title, body)
     time.sleep(check_delay)
